5ed5185eca1418639e744f0819c2cf793f7b54fa
Amit Kumar <amitkuamr981>
2025-07-22 06:34:03 +0530

    add BasicChatBotNode.py

--- src/LangGraph_agentic_ai/nodes/basic_chatbot_node.py
+++ src/LangGraph_agentic_ai/nodes/basic_chatbot_node.py
@@ -1,1 +1,15 @@
 
+from src.LangGraph_agentic_ai.state.state import state
+
+
+class BasicChatbotNode:
+    "implimenting basic chatbot"
+
+    def __init__(self,model):
+        self.llm=model
+    
+    def process(self):
+       "process the input and generate the llm response"
+       return {"messages":self.llm.invoke(state['messages'])}
+    
+        

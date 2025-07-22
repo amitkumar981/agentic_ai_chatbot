243a6ea4a8b28b90199d14249ea09ba468838ff3
Amit Kumar <amitkuamr981>
2025-07-22 09:01:32 +0530

    git add graph_builder.py

--- src/LangGraph_agentic_ai/state/state.py
+++ src/LangGraph_agentic_ai/state/state.py
@@ -4,7 +4,7 @@ from langgraph.graph.message import add_messages
 from typing import Annotated
 
 #define class
-class state(TypedDict):
+class State(TypedDict):
     """class represents the state of the input to assistant"""
     messages:Annotated[List,add_messages]  
 
